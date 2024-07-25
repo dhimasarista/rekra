2781158d7a8195c4b8c402b438f4c470c8c059f7
Dhimas Arista <dhimasharrisst@gmail.com>
2024-07-25 17:06:46 +0700

    test: update PasswordTest.php

--- tests/Unit/PasswordTest.php
+++ tests/Unit/PasswordTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use PHPUnit\Framework\TestCase;
 
@@ -13,7 +14,8 @@ class PasswordTest extends TestCase
      */
     public function test_example(): void
     {
-        $user = User::where("username", "master")->first();
+
+        $user = DB::table('users')->where("username", "master")->first();
         $checkPassword = Hash::check("soliddd45", $user->password);
         $this->assertTrue($checkPassword);
 
