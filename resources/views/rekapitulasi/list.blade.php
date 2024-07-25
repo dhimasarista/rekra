b675b2e9a1cb7b547bf0889e536e951025675f56
dhimasarista <dhimasharrisst@gmail.com>
2024-07-25 21:57:50 +0700

    change action button

--- resources/views/rekapitulasi/list.blade.php
+++ resources/views/rekapitulasi/list.blade.php
@@ -36,22 +36,9 @@
                                     <tr>
                                         <td>{{ $counter++ }}</td>
                                         <td>{{ Formatting::capitalize($d->calon_name . ' - ' . $d->wakil_name) }}</td>
-                                        <td>12</td>
+                                        <td>400.000</td>
                                         <td>
-                                            <div class="dropdown">
-                                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
-                                                    href="#" role="button" data-toggle="dropdown">
-                                                    <i class="dw dw-more"></i>
-                                                </a>
-                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-                                                    <a class="dropdown-item" href="#"><i class="dw dw-eye"></i>
-                                                        Lihat</a>
-                                                    <a class="dropdown-item" href="#"><i class="dw dw-edit2"></i>
-                                                        Edit</a>
-                                                    <a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i>
-                                                        Delete</a>
-                                                </div>
-                                            </div>
+                                            <a href="#" style="text-decoration: underline">Detail</a>
                                         </td>
                                     </tr>
                                 @endforeach
