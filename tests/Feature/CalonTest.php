1d04ce906ded8e38a88299368266e9d6f0d6b62f
Dhimas Arista <dhimasharrisst@gmail.com>
2024-07-25 16:42:32 +0700

    test: update CalonTest.php

--- tests/Feature/CalonTest.php
+++ tests/Feature/CalonTest.php
@@ -13,7 +13,7 @@ class CalonTest extends TestCase
      */
     public function test_example(): void
     {
-        $response = $this->delete('/calon?Id=0190df21-4815-7172-8602-7856a0020abb');
+        $response = $this->get('/calon');
         // $response = $this->get(route("calon.destroy", ["Id" => '0190df21-4815-7172-8602-7856a0020abb']));
         $response->assertStatus(200);
     }
