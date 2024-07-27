b454e4bd37e75708565c43d8f7dd2cb00122eda9
Dhimas Arista <dhimasharrisst@gmail.com>
2024-07-27 15:13:35 +0700

    chore: delete Regex.php

--- app/Helpers/Regex.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Helpers;
-use Dotenv\Util\Regex;
-
-class RegexValidation {
-    public static function isUsernameValid($username){
-        return Regex::matches("^[a-zA-Z0-9]+$", $username);
-    }
-}
