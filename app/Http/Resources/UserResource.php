fc3e12a52b5967fa3cd642aaed186a3003199a98
dhimasarista <dhimasharrisst@gmail.com>
2024-08-24 15:25:40 +0700

    feat: create UserResource.php

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "username" => $this->username,
+            "remember_token" => $this->remember_token,
+            "password" => "",
+            "is_active" => $this->is_active,
+            "code" => $this->code,
+            "level" => $this->level,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+            "deleted_at" => $this->deleted_at,
+        ];
+    }
+}
