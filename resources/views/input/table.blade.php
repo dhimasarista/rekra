33608b7aac36a88d30c64b21d7c970b2a9163de9
dhimasarista <dhimasharrisst@gmail.com>
2024-08-25 23:37:52 +0700

    table resposive

--- resources/views/input/table.blade.php
+++ resources/views/input/table.blade.php
@@ -18,7 +18,7 @@
                     </div>
                     @use('App\Helpers\Formatting')
                     <div class="pb-20">
-                        <table id="init-table" class="table hover stripe multiple-select-row data-table-export wrap">
+                        <table id="init-table" class="table hover stripe multiple-select-row data-table-export wrap responsive">
                             <thead>
                                 <tr>
                                     <th>Nama</th>
