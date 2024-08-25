9da9c87d41d5c2ea9cd45e87c0df6a928c7b3f77
dhimasarista <dhimasharrisst@gmail.com>
2024-08-25 22:22:17 +0700

    update RoleRedirect.php

--- app/Http/Middleware/RoleRedirect.php
+++ app/Http/Middleware/RoleRedirect.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\KabKota;
+use App\Models\Provinsi;
 use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
@@ -20,16 +21,23 @@ class RoleRedirect
         $username = $request->session()->get('username');
         $user = User::where("username", $username)->first();
         // Query Parameters
-        $queryType = $request->query("Type");
-        $queryId = $request->query("Id");
+        $typeQuery = $request->query("Type");
+        $idQuery = $request->query("Id");
         $queryParams = $request->query();
 
         // user role
         $userRole = $user->level == "kabkota" || $user->level == "provinsi";
         // mencegah user mengakses id lain jika bukan ranahnya
-        if ($userRole && $queryType == "Kabkota") {
-            if ($user->code != $queryId) {
+        if ($userRole && $typeQuery == "Kabkota") {
+            if ($user->code != $idQuery) {
                 $queryParams["Id"] = $user->code;
+                $newUrl = $request->url()."?".http_build_query($queryParams);
+                return redirect($newUrl);
+            }
+        } else if ($userRole && $typeQuery == "Provinsi") {
+            $kabkota = Kabkota::where("id", $user->code)->first();
+            if ($kabkota->provinsi_id != $idQuery) {
+                $queryParams["Id"] = $kabkota->provinsi_id;
                 $newUrl = $request->url()."?".http_build_query($queryParams);
                 return redirect($newUrl);
             }
