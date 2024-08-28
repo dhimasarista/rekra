9d6d34b75d81fa845a2da3ec4be88ccf10798809
Dhimas Arista <dhimasharrisst@gmail.com>
2024-08-28 12:39:57 +0700

    update LoginHistory.php

--- app/Models/LoginHistory.php
+++ app/Models/LoginHistory.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LoginHistory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
     protected $table = 'login_histories';
     protected $fillable = [
         'user_id',
