1b402877b80fb922b710620febdf1e3d757d93af
dhimasarista <dhimasharrisst@gmail.com>
2024-09-01 18:17:57 +0700

    change images

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -65,7 +65,7 @@
 
         .LoginPageInnerContainer .ImageContianer {
             width: 50%;
-            background-image: url('https://images.unsplash.com/photo-1604451372323-158c45058d10?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+            background-image: url('https://images.unsplash.com/photo-1666875753105-c63a6f3bdc86?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
             background-size: cover;
             background-position: center;
             min-height: 100%;
