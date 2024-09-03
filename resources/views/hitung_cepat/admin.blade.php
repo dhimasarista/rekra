d1ebde98f4bc00b7894cb605610de328ad61ed70
dhimasarista <dhimasharrisst@gmail.com>
2024-09-03 19:13:06 +0700

    update admin.blade.php

--- resources/views/hitung_cepat/admin.blade.php
+++ resources/views/hitung_cepat/admin.blade.php
@@ -5,56 +5,61 @@
     @endphp
     @use('App\Helpers\Formatting')
     <div class="xs-pd-20-10 pd-ltr-20">
-        < class="title pb-20 d-flex justify-content-between align-items-center">
-            <div class="row">
-                <div class="col-md-6">
-                    <select class="custom-select form-control">
-                        <option disabled selected="">Tingkat</option>
-                        <option value="1">Provinsi</option>
-                        <option value="2">KabKota</option>
-                    </select>
-                </div>
-                <div class="col-md-6">
-                    <select class="custom-select">
-                        <option disabled selected="">Kab/Kota</option>
-                        <option value="1">One</option>
-                    </select>
-                </div>
-            </div>
+        <div class="title pb-20 d-flex justify-content-between align-items-center">
+            <h2 class="h2 mb-0">{{ Formatting::capitalize($segments[0]) }}</h2>
             <div class="text-right">
-
-            </div>
-            <div class="row pb-10">
-                <div class="col-md-12 mb-20">
-                    <!-- Export Datatable start -->
-                    <div class="card-box mb-30">
-                        <div class="pd-20">
-                            <h4 class="text-blue h4">Hitung Cepat</h4>
-                        </div>
-                        <div class="pb-20">
-                            <table id="datatable-table"
-                                class="table hover stripe multiple-select-row data-table-export wrap">
-                                <thead>
-                                    <tr>
-                                        <th>Username</th>
-                                        <th>Login Time</th>
-                                        <th>IP Address</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <script>
-                        $("#datatable-table").DataTable({})
-                    </script>
-                    <!-- Export Datatable End -->
-                </div>
-                <div class="col-md-4 mb-20">
-
-                </div>
+                <a class="btn btn-sm btn-dark m-1" href="{{ route('wilayah.form', ['Type' => 'Kabkota']) }}">
+                    <i class="fa fa-plus"></i> KabKota
+                </a>
+                <a class="btn btn-sm btn-dark m-1" href="{{ route('wilayah.form', ['Type' => 'Kecamatan']) }}">
+                    <i class="fa fa-plus"></i> Kecamatan
+                </a>
+                <a class="btn btn-sm btn-dark m-1" href="{{ route('wilayah.form', ['Type' => 'Kelurahan']) }}">
+                    <i class="fa fa-plus"></i> Kelurahan
+                </a>
+                <a id="tambah-tps" class="btn btn-sm btn-dark m-1" href="javascript:;">
+                    <i class="fa fa-plus"></i> TPS
+                </a>
             </div>
         </div>
-    @endsection
+        <div class="row pb-10">
+            <div class="col-md-12 mb-20">
+                <!-- Export Datatable start -->
+                <div class="card-box mb-30">
+                    <div class="pd-20">
+                        <h4 class="text-blue h4">Hello</h4>
+                    </div>
+                    <div class="pb-20">
+                        <table id="datatable-table" class="table hover stripe multiple-select-row data-table-export wrap">
+                            <thead>
+                                <tr>
+                                    <th>Hello</th>
+                                    <th>Hello</th>
+                                    <th>Hello</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {{-- <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr> --}}
+                            </tbody>
+                        </table>
+                    </div>
+                    <script>
+                        $("#datatable-table").DataTable({
+                            "order": []
+                        })
+                    </script>
+                </div>
+                <!-- Export Datatable End -->
+            </div>
+            <div class="col-md-4 mb-20">
+
+            </div>
+        </div>
+    </div>
+    <script src="../admin/src/plugins/apexcharts/apexcharts.min.js"></script>
+    <script src="../admin/vendors/scripts/dashboard3.js"></script>
+@endsection
