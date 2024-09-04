73e7482688416038978db9908187fc014f2436fd
dhimasarista <dhimasharrisst@gmail.com>
2024-09-05 01:14:31 +0700

    update HitungSuaraCepatAdmin.php and HitungSuaraCepatAdminDetail.php

--- app/Models/HitungSuaraCepatAdminDetail.php
+++ app/Models/HitungSuaraCepatAdminDetail.php
@@ -4,8 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class HitungSuaraCepatAdminDetail extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $primaryKey = "id";
+    public $incrementing = false;
+    protected $table = 'hitung_suara_cepat_admin_detail';
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        "id",
+        "tps_id",
+        "calon_id"
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            /**
+             * Memeriksa dan membuat primary key menjadi unique id
+            */
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Uuid::uuid7();
+            }
+        });
+        static::saving(function ($model) {
+            if (!$model->exists) {
+                $model->created_at = $model->freshTimestamp();
+            }
+            $model->updated_at = $model->freshTimestamp();
+        });
+    }
+    public function tps(): BelongsTo {
+        return $this->belongsTo(Tps::class);
+    }
+    public function calon(): BelongsTo {
+        return $this->belongsTo(Calon::class);
+    }
 }
