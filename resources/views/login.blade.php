619ac8c1b532a610b89db499730ac047e3271db7
Dhimas Arista <dhimasharrisst@gmail.com>
2024-09-05 16:15:22 +0700

    update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -65,7 +65,7 @@
 
         .LoginPageInnerContainer .ImageContianer {
             width: 50%;
-            background-image: url('https://images.unsplash.com/photo-1666875753105-c63a6f3bdc86?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+            background-image: url('https://images.prismic.io/turing/652ec7adfbd9a45bcec81a41_Laravel_1f3f0a9d0c.webp?auto=format%2Ccompress&fit=max&w=3840');
             background-size: cover;
             background-position: center;
             min-height: 100%;
