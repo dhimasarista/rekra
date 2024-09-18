d0087edc6bd320e1590660d76d4731fb0db8e051
Dhimas Arista <dhimasharrisst@gmail.com>
2024-09-18 16:27:45 +0700

    update 2024_09_01_124505_create_hitung_suara_cepat_saksis_table.php add nik

--- database/migrations/2024_09_01_124505_create_hitung_suara_cepat_saksis_table.php
+++ database/migrations/2024_09_01_124505_create_hitung_suara_cepat_saksis_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +12,7 @@ return new class extends Migration
     {
         Schema::create('hitung_suara_cepat_saksi', function (Blueprint $table) {
             $table->uuid("id")->primary();
+            $table->char("nik", 16)->unique();
             $table->text("updated_by")->default("");
             $table->timestamps();
             $table->softDeletes();
