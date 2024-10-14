0c9d24890de7d250e2f3bb921fe7da60df012481
dhimasarista <dhimasharrisst@gmail.com>
2024-10-15 02:17:40 +0700

    fixing ui

--- resources/views/data_pemilih/index.blade.php
+++ resources/views/data_pemilih/index.blade.php
@@ -8,7 +8,7 @@
         <div class="title pb-20 d-flex justify-content-between align-items-center">
             <h2 class="h2 mb-0">Data Pemilih</h2>
             <div class="text-right">
-                <a class="btn btn-sm btn-dark text-light m-1">
+                <a class="btn btn-sm btn-dark text-light m-1" data-toggle="modal" data-target="#Medium-modal">
                     <i class="fa fa-plus"></i> Tambah Data
                 </a>
                 <a class="btn btn-sm btn-dark text-light m-1">
@@ -18,10 +18,19 @@
         </div>
         <div class="row pb-10">
             <div class="col-md-12 mb-20">
-                <!-- Export Datatable start -->
+                {{-- data table button --}}
+                <script src="{{ asset('admin/src/plugins/datatables/js/dataTables.buttons.min.js') }}"></script>
+                <script src="{{ asset('admin/src/plugins/datatables/js/buttons.bootstrap4.min.js') }}"></script>
+                <script src="{{ asset('admin/src/plugins/datatables/js/buttons.print.min.js') }}"></script>
+                <script src="{{ asset('admin/src/plugins/datatables/js/buttons.html5.min.js') }}"></script>
+                <script src="{{ asset('admin/src/plugins/datatables/js/buttons.flash.min.js') }}"></script>
+                <script src="{{ asset('admin/src/plugins/datatables/js/pdfmake.min.js') }}"></script>
+                <script src="{{ asset('admin/src/plugins/datatables/js/vfs_fonts.js') }}"></script>
                 <div class="card-box mb-30">
-                    <div class="pd-20">
+                    <div class="pd-20 d-flex justify-content-between align-items-center">
                         <h4 class="text-primary h4">Daftar Pemilih</h4>
+                        <!-- Tombol export akan dimasukkan di sini -->
+                        <div id="export-buttons"></div>
                     </div>
                     <div class="pb-20">
                         <table width="100%" id="datatable-table"
@@ -32,48 +41,89 @@
                                     <th>Nama</th>
                                     <th>Nomor Ponsel</th>
                                     <th>Alamat</th>
-                                    <th>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                    <tr>
-                                        <td>Hello</td>
-                                        <td>{{ Formatting::capitalize("Anto Selang") }}</td>
-                                        <td>Hello</td>
-                                        <td>Hello</td>
-                                        <td>
-                                            <div class="dropdown">
-                                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
-                                                    href="#" role="button" data-toggle="dropdown">
-                                                    <i class="dw dw-more"></i>
-                                                </a>
-                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-                                                    <a class="dropdown-item" href="#"><i class="dw dw-eye"></i>
-                                                        Lihat</a>
-                                                    <a class="dropdown-item"
-                                                        href="#"><i
-                                                            class="dw dw-edit2"></i>
-                                                        Edit</a>
-                                                    <a class="dropdown-item"
-                                                        href="#"><i class="dw dw-delete-3"></i>
-                                                        Hapus</a>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td>Hello</td>
+                                    <td>{{ Formatting::capitalize('Anto Selang') }}</td>
+                                    <td>Hello</td>
+                                    <td>Hello</td>
+                                </tr>
+                                <tr>
+                                    <td>Hello</td>
+                                    <td>{{ Formatting::capitalize('Anto Selang') }}</td>
+                                    <td>Hello</td>
+                                    <td>Hello</td>
+                                </tr>
+                                <tr>
+                                    <td>Hello</td>
+                                    <td>{{ Formatting::capitalize('Anto Selang') }}</td>
+                                    <td>Hello</td>
+                                    <td>Hello</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
+
                 <script>
-                    $("#datatable-table").DataTable({
-                        // "scrollX": true,
-                    })
+                    $(document).ready(function() {
+                        var table = $("#datatable-table").DataTable({
+                            scrollCollapse: true,
+                            autoWidth: false,
+                            responsive: true,
+                            columnDefs: [{
+                                targets: "datatable-nosort",
+                                orderable: false,
+                            }],
+                            "lengthMenu": [
+                                [10, 25, 50, 100, -1],
+                                [10, 25, 50, 100, "All"]
+                            ],
+                            "language": {
+                                "info": "_START_-_END_ of _TOTAL_ entries",
+                                searchPlaceholder: "Search",
+                                lengthMenu: "Show _MENU_ entries",
+                                paginate: {
+                                    next: '<i class="ion-chevron-right"></i>',
+                                    previous: '<i class="ion-chevron-left"></i>'
+                                }
+                            },
+                            dom: '<"d-flex justify-content-between"lBf>rt<"d-flex justify-content-between"ip>',
+                            buttons: [
+                                'copy', 'csv', 'pdf', 'print', 'excel'
+                            ]
+                        });
+
+                        table.buttons().container().appendTo('#export-buttons');
+                    });
                 </script>
-                <!-- Export Datatable End -->
             </div>
             <div class="col-md-4 mb-20">
 
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="Medium-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myLargeModalLabel">Large modal</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                </div>
+                <div class="modal-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,18 +6,18 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <title>Kamar Juang</title>
-    
+
         <!-- Site favicon -->
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('admin/vendors/images/apple-touch-icon.png') }}" />
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('admin/vendors/images/favicon-32x32.png') }}" />
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('admin/vendors/images/favicon-16x16.png') }}" />
-    
+
         <!-- Mobile Specific Metas -->
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    
+
         <!-- Google Font -->
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
-        
+
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="{{ asset('admin/vendors/styles/core.css') }}" />
         <link rel="stylesheet" type="text/css" href="{{ asset('admin/vendors/styles/icon-font.min.css') }}" />
@@ -26,7 +26,7 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('admin/vendors/styles/style.css') }}" />
         <link rel="stylesheet" type="text/css" href="{{ asset('admin/src/plugins/switchery/switchery.min.css') }}" />
         <link rel="stylesheet" type="text/css" href="{{ asset('admin/src/plugins/TopLoaderService/TopLoaderService.css') }}" />
-    
+
         <!-- JavaScript -->
         <script src="{{ asset('admin/src/plugins/TopLoaderService/TopLoaderService.js') }}"></script>
         <script src="{{ asset('admin/vendors/scripts/core.js') }}"></script>
@@ -39,26 +39,18 @@
         <script src="{{ asset('admin/src/plugins/datatables/js/responsive.bootstrap4.min.js') }}"></script>
         <script src="{{ asset('admin/src/plugins/sweetalert2/sweetalert2.js') }}"></script>
         <script src="{{ asset('admin/js/script.js') }}"></script>
-        {{-- data table button --}}
-        <script src="{{ asset("admin/src/plugins/datatables/js/dataTables.buttons.min.js") }}"></script>
-        <script src="{{ asset("admin/src/plugins/datatables/js/buttons.bootstrap4.min.js")}}"></script>
-        <script src="{{ asset("admin/src/plugins/datatables/js/buttons.print.min.js")}}"></script>
-        <script src="{{ asset("admin/src/plugins/datatables/js/buttons.html5.min.js")}}"></script>
-        <script src="{{ asset("admin/src/plugins/datatables/js/buttons.flash.min.js")}}"></script>
-        <script src="{{ asset("admin/src/plugins/datatables/js/pdfmake.min.js")}}"></script>
-        <script src="{{ asset("admin/src/plugins/datatables/js/vfs_fonts.js")}}"></script>
-    
+
         <!-- Socket.io -->
         {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script> --}}
         {{-- <script>
             window.socketIOUrl = "http://localhost:2002";
             const socket = io(window.socketIOUrl);
         </script> --}}
-    
+
         <noscript>
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXZMQSS" height="0" width="0" style="display: none; visibility: hidden"></iframe>
         </noscript>
-    </head>    
+    </head>
 
 <body class="sidebar-dark">
     {{-- Header --}}
